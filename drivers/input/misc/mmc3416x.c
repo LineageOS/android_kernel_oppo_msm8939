76fe1b35464a39da1c90c9aa2d83a8726d63f6ac
Bingzhe Cai <bingzhec@codeaurora.org>
2015-01-09 19:54:00 +0800

    input: sensors: create dedicate workqueue for sensors
    Default system workqueue may be bounded on specific cpu, that cpu
    will always be wakeup to handle the workqueue, this will consume
    extra power in some case. This change fix issue by create dedicate
    sensor workqueue which not bounded to any cpu.
    
    [mikeioannina]: Adjust original commit to oppo sensors only
    
    Change-Id: I38f64def943625cc525b56e863a4ef5384bc10ac
    Signed-off-by: Bingzhe Cai <bingzhec@codeaurora.org>

--- drivers/input/misc/mmc3416x.c
+++ drivers/input/misc/mmc3416x.c
@@ -11,7 +11,7 @@
  *************************************************************/
 
 /*
- * Copyright (c) 2014, Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2015, Linux Foundation. All rights reserved.
  * Linux Foundation chooses to take subject only to the GPLv2 license
  * terms, and distributes only under these terms.
  * Copyright (C) 2010 MEMSIC, Inc.
@@ -122,6 +122,7 @@ struct mmc3416x_vec {
 struct mmc3416x_data {
 	struct mutex		ecompass_lock;
 	struct mutex		ops_lock;
+	struct workqueue_struct *data_wq;
 	struct delayed_work	dwork;
 	struct sensors_classdev	cdev;
 	struct mmc3416x_vec	last;
@@ -301,7 +302,8 @@ static void mmc3416x_poll(struct work_struct *work)
 	input_sync(memsic->idev);
 
 exit:
-	schedule_delayed_work(&memsic->dwork,
+	queue_delayed_work(memsic->data_wq,
+			&memsic->dwork,
 			msecs_to_jiffies(memsic->poll_interval));
 }
 
@@ -590,7 +592,8 @@ static int mmc3416x_set_enable(struct sensors_classdev *sensors_cdev,
 
 		memsic->timeout = jiffies;
 		if (memsic->auto_report)
-			schedule_delayed_work(&memsic->dwork,
+			queue_delayed_work(memsic->data_wq,
+				&memsic->dwork,
 				msecs_to_jiffies(memsic->poll_interval));
 	} else if ((!enable) && memsic->enable) {
 		if (memsic->auto_report)
@@ -870,9 +873,16 @@ static int mmc3416x_probe(struct i2c_client *client, const struct i2c_device_id 
 		goto out_init_input;
 	}
 
+	memsic->data_wq = NULL;
 	if (memsic->auto_report) {
-		dev_info(&client->dev, "auto report is enabled\n");
+		dev_dbg(&client->dev, "auto report is enabled\n");
 		INIT_DELAYED_WORK(&memsic->dwork, mmc3416x_poll);
+		memsic->data_wq =
+			create_freezable_workqueue("mmc3416_data_work");
+		if (!memsic->data_wq) {
+			dev_err(&client->dev, "Cannot create workqueue.\n");
+			goto out_create_workqueue;
+		}
 	}
 
 	memsic->cdev = sensors_cdev;
@@ -911,6 +921,9 @@ static int mmc3416x_probe(struct i2c_client *client, const struct i2c_device_id 
 out_power_set:
 	sensors_classdev_unregister(&memsic->cdev);
 out_register_classdev:
+	if (memsic->data_wq)
+		destroy_workqueue(memsic->data_wq);
+out_create_workqueue:
 	input_unregister_device(memsic->idev);
 out_init_input:
 out_check_device:
@@ -924,6 +937,8 @@ static int mmc3416x_remove(struct i2c_client *client)
 	struct mmc3416x_data *memsic = dev_get_drvdata(&client->dev);
 
 	sensors_classdev_unregister(&memsic->cdev);
+	if (memsic->data_wq)
+		destroy_workqueue(memsic->data_wq);
 	mmc3416x_power_deinit(memsic);
 
 	if (memsic->idev)
@@ -970,7 +985,8 @@ static int mmc3416x_resume(struct device *dev)
 		}
 
 		if (memsic->auto_report)
-			schedule_delayed_work(&memsic->dwork,
+			queue_delayed_work(memsic->data_wq,
+				&memsic->dwork,
 				msecs_to_jiffies(memsic->poll_interval));
 	}
 
