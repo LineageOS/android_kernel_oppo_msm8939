45a1b9b62360b9c6b2d74eed5e1deff25d668ec3
Danny Baumann <dannybaumann@web.de>
2015-09-08 09:10:13 +0200

    Changes for victara tfa9890 driver.
    Change-Id: I651d4309e9959d13ef87a579a39abc98a8f58f33

--- sound/soc/msm/msm8x16.c
+++ sound/soc/msm/msm8x16.c
@@ -69,9 +69,6 @@ static int msm_proxy_rx_ch = 2;
 /*OPPO 2014-07-21 zhzhyon Add for quat i2s patch*/
 static atomic_t quat_mi2s_clk_ref;
 /*OPPO 2014-07-21 zhzhyon Add end*/
-/*OPPO 2014-08-20 zhzhyon Add for share software*/
-static struct regulator * tfa9890_vdd = NULL;
-/*OPPO 2014-08-20 zhzhyon Add end*/
 
 #ifdef VENDOR_EDIT
 //John.Xu@PhoneSw.AudioDriver, 2015/04/30, Add for 15025 headset compatible
@@ -358,33 +355,25 @@ static int msm_pri_rx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	return 0;
 }
 
-/*OPPO 2014-07-23 zhzhyon Add for tfa9890*/
-static int msm8939_mi2s_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
-            struct snd_pcm_hw_params *params)
+static int msm_be_tfa9890_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
+				struct snd_pcm_hw_params *params)
 {
-    struct snd_interval *rate = hw_param_interval(params,
-    SNDRV_PCM_HW_PARAM_RATE);
+	struct snd_interval *rate = hw_param_interval(params,
+					SNDRV_PCM_HW_PARAM_RATE);
 
-    struct snd_interval *channels = hw_param_interval(params,
-    SNDRV_PCM_HW_PARAM_CHANNELS);
+	struct snd_interval *channels =
+	    hw_param_interval(params, SNDRV_PCM_HW_PARAM_CHANNELS);
 
-    pr_debug("%s rate->min %d rate->max %d channels->min %u channels->max %u ()\n", __func__,
-            rate->min, rate->max, channels->min, channels->max);
+	pr_debug("%s()\n", __func__);
 
-	//
+	rate->min = rate->max = 48000;
+	param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT, SNDRV_PCM_FORMAT_S16_LE);
 
-	param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT,
-					SNDRV_PCM_FORMAT_S16_LE);
+	if (! channels->min)
+		channels->min = channels->max = 2;
 
-	//
-
-    rate->min = rate->max = 48000;
-    channels->min = channels->max = 1;
-
-    return 0;
+	return 0;
 }
-
-/*OPPO 2014-07-23 zhzhyon Add end*/
 
 static int msm_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 				struct snd_pcm_hw_params *params)
@@ -2283,17 +2272,17 @@ static struct snd_soc_dai_link msm8x16_dai[] = {
 		.cpu_dai_name = "msm-dai-q6-mi2s.3",
 		.platform_name = "msm-pcm-routing",
 		/*OPPO 2014-07-21 zhzhyon Modify for tfa9890*/
-		#if 1
+		#if 0
 		.codec_dai_name = "snd-soc-dummy-dai",
 		.codec_name = "snd-soc-dummy",
 		#else
-              .codec_name     = "tfa9890.3-0036",
-              .codec_dai_name = "tfa9890_codec",
+		.codec_name     = "tfa9890.3-0036",
+		.codec_dai_name = "tfa9890_codec_left",
 		#endif
 		/*OPPO 2014-07-21 zhzhyon Modify end*/
 		.no_pcm = 1,
 		.be_id = MSM_BACKEND_DAI_QUATERNARY_MI2S_RX,
-		.be_hw_params_fixup = msm8939_mi2s_be_hw_params_fixup,//msm_be_hw_params_fixup,
+		.be_hw_params_fixup = msm_be_tfa9890_hw_params_fixup,
 		.ops = &msm8x16_quat_mi2s_be_ops,
 		.ignore_pmdown_time = 1, /* dai link has playback support */
 		.ignore_suspend = 1,
@@ -3120,97 +3109,7 @@ static int msm8x16_asoc_machine_probe(struct platform_device *pdev)
 
 	if (ret)
 		goto err;
-/*OPPO 2014-07-24 zhzhyon Add for tfa9890*/
-	/*OPPO 2014-08-20 zhzhyon Modify for share software*/
-	/*if( is_project(OPPO_14005))
-	{
-		pdata->audio_vdd_en_gpio = of_get_named_gpio(pdev->dev.of_node,
-						"audio-vdd-enable-gpios", 0);
-		if (pdata->audio_vdd_en_gpio < 0)
-		{
-			dev_err(&pdev->dev,
-				"property %s in node %s not found %d\n",
-				"audio-vdd-enable-gpios", pdev->dev.of_node->full_name,
-				pdata->audio_vdd_en_gpio);
-		}
 
-		pdata->tfa9890_rst = of_get_named_gpio(pdev->dev.of_node,
-						"nxp,reset-gpio", 0);
-		if (pdata->tfa9890_rst < 0)
-		{
-			dev_err(&pdev->dev,
-				"property %s in node %s not found %d\n",
-				"nxp,reset-gpio", pdev->dev.of_node->full_name,
-				pdata->tfa9890_rst);
-		}
-
-
-		pdata->spk_rec_sw = of_get_named_gpio(pdev->dev.of_node,
-						"spk-rec-sw-gpios", 0);
-		if (pdata->audio_vdd_en_gpio < 0)
-		{
-			dev_err(&pdev->dev,
-				"property %s in node %s not found %d\n",
-				"spk-rec-sw-gpios", pdev->dev.of_node->full_name,
-				pdata->spk_rec_sw);
-		}
-
-		if (gpio_is_valid(pdata->audio_vdd_en_gpio))
-		{
-			gpio_direction_output(pdata->audio_vdd_en_gpio, 1);
-		}
-		if (gpio_is_valid(pdata->tfa9890_rst))
-		{
-			gpio_direction_output(pdata->tfa9890_rst, 0);
-		}
-
-		if (gpio_is_valid(pdata->spk_rec_sw))
-		{
-			gpio_direction_output(pdata->spk_rec_sw, 0);
-		}
-	}
-	else */
-
-	/*OPPO 2015-05-11 hanqing.wang Add for reason OPPO_15011 15062 OPPO_MSM_15062*/
-/*huqiao@EXP.BasicDrv.Basic add for clone 15085*/
-	if((is_project(OPPO_14045) || is_project(OPPO_15018)|| is_project(OPPO_15011) || is_project(OPPO_15022) || is_project(OPPO_15085))  && (tfa9890_vdd == NULL))
-	{
-		pdata->tfa9890_rst = of_get_named_gpio(pdev->dev.of_node,
-						"nxp,reset-gpio", 0);
-		if (pdata->tfa9890_rst < 0)
-		{
-			dev_err(&pdev->dev,
-				"property %s in node %s not found %d\n",
-				"nxp,reset-gpio", pdev->dev.of_node->full_name,
-				pdata->tfa9890_rst);
-		}
-		if (gpio_is_valid(pdata->tfa9890_rst))
-		{
-			gpio_request(pdata->tfa9890_rst,"tfa9890_rst");
-			gpio_direction_output(pdata->tfa9890_rst, 0);
-		}
-
-
-		tfa9890_vdd = regulator_get(&pdev->dev, "tfa9890_vdd");
-		if(tfa9890_vdd)
-		{
-			if( regulator_count_voltages(tfa9890_vdd) > 0)
-			{
-				regulator_set_voltage(tfa9890_vdd, 1800000, 1800000);
-			}
-
-			ret = regulator_enable(tfa9890_vdd);
-			if(ret)
-			{
-				dev_err(&pdev->dev,
-					"Regulator vdd enable failed ret=%d\n", ret);
-				regulator_disable(tfa9890_vdd);
-				tfa9890_vdd = NULL;
-
-			}
-		}
-	}
-    /*OPPO 2014-07-24 zhzhyon Add end*/
 	ret = msm8x16_populate_dai_link_component_of_node(card);
 	if (ret) {
 		ret = -EPROBE_DEFER;
@@ -3287,9 +3186,6 @@ static int msm8x16_asoc_machine_remove(struct platform_device *pdev)
 		iounmap(pdata->vaddr_gpio_mux_mic_ctl);
 	snd_soc_unregister_card(card);
 	mutex_destroy(&pdata->cdc_mclk_mutex);
-	/*OPPO 2014-08-20 zhzhyon Add for tfa9890*/
-	regulator_disable(tfa9890_vdd);
-	/*OPPO 2014-08-20 zhzhyon Add end*/
 	return 0;
 }
 
