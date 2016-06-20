03ca116113e215a678a9ab0a592f47504e71aed8
Danny Baumann <dannybaumann@web.de>
2016-06-20 10:40:16 +0200

    mdss: Add some R5 project checks from R5 5.1 kernel.
    Change-Id: I0f36d4e6132c533ecf9caeb0c9d9c6986d9d4986

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -485,7 +485,7 @@ static int mdss_dsi_off(struct mdss_panel_data *pdata, int power_state)
 #ifdef VENDOR_EDIT
 	/* nanwei.deng@Mobile Phone Software bsp.Driver, 2014/10/30  Add for 14045 charger */
 /*huqiao@EXP.BasicDrv.Basic add for clone 15085*/
-	if (is_project(OPPO_15009) || is_project(OPPO_15037) || is_project(OPPO_15018) || is_project(OPPO_15022) || is_project(OPPO_14045) || is_project(OPPO_15005)|| is_project(OPPO_15011) || is_project(OPPO_15085))
+	if (is_project(OPPO_14005) || is_project(OPPO_15009) || is_project(OPPO_15037) || is_project(OPPO_15018) || is_project(OPPO_15022) || is_project(OPPO_14045) || is_project(OPPO_15005)|| is_project(OPPO_15011) || is_project(OPPO_15085))
 	{
 		opchg_check_lcd_off();
 	}
@@ -582,7 +582,7 @@ int mdss_dsi_on(struct mdss_panel_data *pdata)
 #ifdef VENDOR_EDIT
 	/* nanwei.deng@Mobile Phone Software bsp.Driver, 2014/10/30  Add for 14045 charger */
 /*huqiao@EXP.BasicDrv.Basic add for clone 15085*/
-	if (is_project(OPPO_15009) || is_project(OPPO_15037) || is_project(OPPO_15018) || is_project(OPPO_15022) || is_project(OPPO_14045) || is_project(OPPO_15005)|| is_project(OPPO_15011) || is_project(OPPO_15085))
+	if (is_project(OPPO_14005) || is_project(OPPO_15009) || is_project(OPPO_15037) || is_project(OPPO_15018) || is_project(OPPO_15022) || is_project(OPPO_14045) || is_project(OPPO_15005)|| is_project(OPPO_15011) || is_project(OPPO_15085))
 	{
 		opchg_check_lcd_on();
 	}
