07acdf77f50f93840da958afed03029121a8a2ba
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-09-26 17:45:48 +0300

    camera_v2: Roll back to LA.BR.1.1.3.c8 stack
    Change-Id: I5b9047cba7792d6e8d154f5a10a137b60ecabd4d

--- include/media/msm_camsensor_sdk.h
+++ include/media/msm_camsensor_sdk.h
@@ -20,7 +20,6 @@
 #define CSI_DECODE_DPCM_10_8_10 5
 #define MAX_CID                 16
 #define I2C_SEQ_REG_DATA_MAX    256
-#define I2C_REG_DATA_MAX       (8*1024)
 #define MSM_V4L2_PIX_FMT_META v4l2_fourcc('M', 'E', 'T', 'A') /* META */
 
 #define MAX_ACTUATOR_REG_TBL_SIZE 8
@@ -29,7 +28,7 @@
 #define MAX_ACTUATOR_SCENARIO     8
 #define MAX_ACT_MOD_NAME_SIZE     32
 #define MAX_ACT_NAME_SIZE         32
-#define MAX_ACTUATOR_INIT_SET     32
+#define MAX_ACTUATOR_INIT_SET     12
 #define MAX_I2C_REG_SET           12
 
 #define MAX_NAME_SIZE             32
@@ -62,20 +61,6 @@ enum msm_sensor_power_seq_type_t {
 	SENSOR_VREG,
 	SENSOR_I2C_MUX,
 	SENSOR_I2C,
-};
-
-enum msm_camera_qup_i2c_write_batch_size_t {
-	MSM_CAMERA_I2C_BATCH_SIZE_1 = 1,
-	MSM_CAMERA_I2C_BATCH_SIZE_2,
-	MSM_CAMERA_I2C_BATCH_SIZE_3,
-	MSM_CAMERA_I2C_BATCH_SIZE_4,
-	MSM_CAMERA_I2C_BATCH_SIZE_5,
-	MSM_CAMERA_I2C_BATCH_SIZE_MAX,
-};
-
-enum msm_camera_qup_i2c_write_batch_t {
-	MSM_CAMREA_I2C_BATCH_DISABLE = 0,
-	MSM_CAMERA_I2C_BATCH_ENABLE,
 };
 
 enum msm_camera_i2c_reg_addr_type {
@@ -164,6 +149,10 @@ enum actuator_type {
 	ACTUATOR_VCM,
 	ACTUATOR_PIEZO,
 	ACTUATOR_HVCM,
+#ifdef CONFIG_MACH_OPPO
+/*zhengrong.zhang 2015-01-23 Add for VCM */
+	ACTUATOR_HALL_EFFECT,
+#endif
 };
 
 enum msm_flash_driver_type {
@@ -179,12 +168,6 @@ enum msm_flash_cfg_type_t {
 	CFG_FLASH_OFF,
 	CFG_FLASH_LOW,
 	CFG_FLASH_HIGH,
-};
-
-enum msm_sensor_output_format_t {
-	MSM_SENSOR_BAYER,
-	MSM_SENSOR_YCBCR,
-	MSM_SENSOR_META,
 };
 
 struct msm_sensor_power_setting {
@@ -233,7 +216,6 @@ struct msm_camera_sensor_slave_info {
 	uint8_t  is_init_params_valid;
 	struct msm_sensor_init_params sensor_init_params;
 	uint8_t is_flash_supported;
-	enum msm_sensor_output_format_t output_format;
 };
 
 struct msm_camera_i2c_reg_array {
@@ -248,7 +230,6 @@ struct msm_camera_i2c_reg_setting {
 	enum msm_camera_i2c_reg_addr_type addr_type;
 	enum msm_camera_i2c_data_type data_type;
 	uint16_t delay;
-	enum msm_camera_qup_i2c_write_batch_t qup_i2c_batch;
 };
 
 struct msm_camera_csid_vc_cfg {
