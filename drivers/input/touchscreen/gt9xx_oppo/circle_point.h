276925caf12fdca72701bcecacfe7c4f2eeba379
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-10-08 19:04:50 +0300

    input: touchscreen: Import OPPO GT9xx touchscreen driver
    * Original code: LA.BR.1.2.6_rb1.7 Spectrum sources
    
    Change-Id: I1f37eee472a0a62881200a60c538f8038fd8d0b0

---
+++ drivers/input/touchscreen/gt9xx_oppo/circle_point.h
@@ -0,0 +1,18 @@
+#ifndef __CIRCLE_POINT_H__
+#define __CIRCLE_POINT_H__
+
+typedef struct
+{
+	int x;
+	int y;
+} Point;
+
+typedef struct
+{
+	Point begin;
+	Point end;
+}PointsCouple;
+
+extern void GetCirclePoints(Point *input_points, int number ,Point  *pPnts);
+extern int cross(Point a, Point b, Point c);
+#endif
