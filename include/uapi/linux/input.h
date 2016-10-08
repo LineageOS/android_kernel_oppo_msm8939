f2f41ec2d3501456b0d945670e20e6ddffca11af
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-10-08 19:06:07 +0300

    input: touchscreen: Import OPPO Synaptics touchscreen driver
    * Original code: LA.BR.1.2.6_rb1.7 Spectrum sources
    
    2015-09-11      Danny Baumann   input: synaptics: Implement gesture API as expected by CM.
    2016-03-16      Keith Mok       input: synaptics: Send KEY_WAKEUP for double-tap event
    2016-03-27      Michael Bestas  input: synaptics: Report correct supported touchkeys
    2016-04-14      Danny Baumann   input: synaptics: Remove incorrect reporting of softkey capabilities.
    2016-12-05      Danny Baumann   input: synaptics: Make gesture enable flags more generic.
    2016-12-06      Danny Baumann   input: synaptics: Stop exporting control files as world-writable.
    2016-12-06      Michael Bestas  input: synaptics: Support all gestures
    
    Change-Id: I68cfd74d4e787d46c6113e8767b9094403c4a531

--- include/uapi/linux/input.h
+++ include/uapi/linux/input.h
@@ -476,6 +476,20 @@ struct input_keymap_entry {
 
 #define KEY_MICMUTE		248	/* Mute / unmute the microphone */
 
+/* Oppo specific keycodes */
+#define KEY_GESTURE_CIRCLE		250
+#define KEY_GESTURE_DOUBLE_SWIPE	251
+#define KEY_GESTURE_DOWN_ARROW		252
+#define KEY_GESTURE_LEFT_ARROW		253
+#define KEY_GESTURE_RIGHT_ARROW		254
+#define KEY_GESTURE_UP_ARROW		255
+#define KEY_GESTURE_SWIPE_RIGHT		256
+#define KEY_GESTURE_SWIPE_LEFT		257
+#define KEY_GESTURE_SWIPE_DOWN		258
+#define KEY_GESTURE_SWIPE_UP		259
+#define KEY_GESTURE_W			260
+#define KEY_GESTURE_M			261
+
 /* Code 255 is reserved for special needs of AT keyboard driver */
 
 #define BTN_MISC		0x100
