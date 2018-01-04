00f0659de770afe94072e067c5fbb1ca03811b17
Michael Bestas <mkbestas@lineageos.org>
2018-01-04 18:29:43 +0200

    Revert "Merge remote-tracking branch 'wolfson/v3.10-arizona' into cm-14.1-caf-8916"
    This reverts commit efb29a0052867b90b36dfe6fd5bfc9e46ad0ccdc, reversing
    changes made to dc256c231a37f988747356606f3d284fa1a6713b.
    
    * Breaks headset audio on OPPO msm8939
    
    Conflicts:
    	include/uapi/sound/compress_params.h
    
    Change-Id: I67fa07d3ba04b76cc2b17fdccd78d4d83d0c815a

--- include/uapi/sound/compress_params.h
+++ include/uapi/sound/compress_params.h
@@ -57,7 +57,6 @@
 #define MAX_NUM_CODECS 32
 #define MAX_NUM_CODEC_DESCRIPTORS 32
 #define MAX_NUM_BITRATES 32
-#define MAX_NUM_SAMPLE_RATES 32
 
 /* compressed TX */
 #define MAX_NUM_FRAMES_PER_BUFFER 1
@@ -300,7 +299,7 @@ struct snd_enc_vorbis {
 	__u32 max_bit_rate;
 	__u32 min_bit_rate;
 	__u32 downmix;
-} __attribute__((packed, aligned(4)));
+};
 
 
 /**
@@ -316,7 +315,7 @@ struct snd_enc_real {
 	__u32 quant_bits;
 	__u32 start_region;
 	__u32 num_regions;
-} __attribute__((packed, aligned(4)));
+};
 
 /**
  * struct snd_enc_flac
@@ -340,28 +339,28 @@ struct snd_enc_real {
 struct snd_enc_flac {
 	__u32 num;
 	__u32 gain;
-} __attribute__((packed, aligned(4)));
+};
 
 struct snd_enc_generic {
 	__u32 bw;	/* encoder bandwidth */
 	__s32 reserved[15];
-} __attribute__((packed, aligned(4)));
+};
 struct snd_dec_ddp {
 	__u32 params_length;
 	__u32 params_id[18];
 	__u32 params_value[18];
-} __attribute__((packed, aligned(4)));
+};
 struct snd_dec_flac {
 	__u16 sample_size;
 	__u16 min_blk_size;
 	__u16 max_blk_size;
 	__u16 min_frame_size;
 	__u16 max_frame_size;
-} __attribute__((packed, aligned(4)));
+};
 
 struct snd_dec_vorbis {
 	__u32 bit_stream_fmt;
-} __attribute__((packed, aligned(4)));
+};
 
 struct snd_dec_alac {
 	__u32 frame_length;
@@ -376,7 +375,7 @@ struct snd_dec_alac {
 	__u32 avg_bit_rate;
 	__u32 sample_rate;
 	__u32 channel_layout_tag;
-} __attribute__((packed, aligned(4)));
+};
 
 struct snd_dec_ape {
 	__u16 compatible_version;
@@ -389,7 +388,7 @@ struct snd_dec_ape {
 	__u16 num_channels;
 	__u32 sample_rate;
 	__u32 seek_table_present;
-} __attribute__((packed, aligned(4)));
+};
 
 union snd_codec_options {
 	struct snd_enc_wma wma;
@@ -402,12 +401,11 @@ union snd_codec_options {
 	struct snd_dec_vorbis vorbis_dec;
 	struct snd_dec_alac alac;
 	struct snd_dec_ape ape;
-} __attribute__((packed, aligned(4)));
+};
 
 /** struct snd_codec_desc - description of codec capabilities
  * @max_ch: Maximum number of audio channels
- * @sample_rates: Sampling rates in Hz, use values like 48000 for this
- * @num_sample_rates: Number of valid values in sample_rates array
+ * @sample_rates: Sampling rates in Hz, use SNDRV_PCM_RATE_xxx for this
  * @bit_rate: Indexed array containing supported bit rates
  * @num_bitrates: Number of valid values in bit_rate array
  * @rate_control: value is specified by SND_RATECONTROLMODE defines.
@@ -429,8 +427,7 @@ union snd_codec_options {
 
 struct snd_codec_desc {
 	__u32 max_ch;
-	__u32 sample_rates[MAX_NUM_SAMPLE_RATES];
-	__u32 num_sample_rates;
+	__u32 sample_rates;
 	__u32 bit_rate[MAX_NUM_BITRATES];
 	__u32 num_bitrates;
 	__u32 rate_control;
@@ -439,7 +436,7 @@ struct snd_codec_desc {
 	__u32 formats;
 	__u32 min_buffer;
 	__u32 reserved[15];
-} __attribute__((packed, aligned(4)));
+};
 
 /** struct snd_codec
  * @id: Identifies the supported audio encoder/decoder.
@@ -448,8 +445,7 @@ struct snd_codec_desc {
  * @ch_out: Number of output channels. In case of contradiction between
  *		this field and the channelMode field, the channelMode field
  *		overrides.
- * @sample_rate: Audio sample rate of input data in Hz, use values like 48000
- *		for this.
+ * @sample_rate: Audio sample rate of input data
  * @bit_rate: Bitrate of encoded data. May be ignored by decoders
  * @rate_control: Encoding rate control. See SND_RATECONTROLMODE defines.
  *               Encoders may rely on profiles for quality levels.
@@ -482,13 +478,13 @@ struct snd_codec {
 	union snd_codec_options options;
 	__u32 flags;
 	__u32 reserved[2];
-} __attribute__((packed, aligned(4)));
+};
 
 struct snd_codec_metadata {
 	__u32 length;
 	__u32 offset;
 	__u64 timestamp;
 	__u32 reserved[4];
-} __attribute__((packed, aligned(4)));
+};
 
 #endif
